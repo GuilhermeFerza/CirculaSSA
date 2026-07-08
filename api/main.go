215f5d6238973f1d659f9c8250a4b4f10f2d4fe7
Guilherme de Souza <guilherme.desouzaferreira2004@gmail.com>
2026-07-08 13:52:38 -0300

    feat: integrate Leaflet for map display and update API data fetching

--- api/main.go
+++ api/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+type Vaga struct {
+	ID        int     `json:"id"`
+	Titulo    string  `json:"titulo"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
 
 func main() {
 
 	r := gin.Default()
 
+	r.Use(cors.Default())
+
 	api := r.Group("/api")
 	{
 		api.GET("/vaga", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"vaga 1": "Cajazeiras",
-				"vaga 2": "Brotas",
-			})
+			vagas := []Vaga{
+				{ID: 1, Titulo: "Estágio em TI - Brotas", Latitude: -12.9866, Longitude: -38.4900},
+				{ID: 2, Titulo: "Atendente de Padaria - Cajazeiras", Latitude: -12.8906, Longitude: -38.4144},
+				{ID: 3, Titulo: "Jovem Aprendiz - Ribeira", Latitude: -12.9099, Longitude: -38.4965},
+			}
+			c.JSON(http.StatusOK, vagas)
 		})
 	}
 	r.Run(":8080")
---
+++ client/src/App.tsx
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+import './App.css'
+
+interface Vaga {
+    id: number;
+    titulo: string;
+    latitude: number;
+    longitude: number;
+}
+
+export default function App(){
+    const [vagas, setVagas] = useState<Vaga[]>([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:8080/api/vaga')
+        .then((resposta) => resposta.json())
+        .then((dados)=>{
+            setVagas(dados)
+        })
+        .catch((erro) => console.error("Erro ao buscar vagas: ", erro))
+    })
+
+    const salvadorCentro: [number, number] = [-12.9714, -38.5014];
+
+    return(
+        <MapContainer center={salvadorCentro} zoom={12} scrollWheelZoom={true}
+        >
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {vagas.map((vaga) => (
+            <Marker key={vaga.id} position={[vaga.latitude, vaga.longitude]}>
+                <Popup>
+                    <strong>{vaga.titulo}</strong>
+                </Popup>
+            </Marker>
+        ))}
+        </MapContainer>
+    )
+
+}
