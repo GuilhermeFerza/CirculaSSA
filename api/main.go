0343e67c6b37d0dd40a281e027c17bed1fa65a45
Guilherme de Souza <guilherme.desouzaferreira2004@gmail.com>
2026-07-16 17:55:28 -0300

    refactor: move Vaga, User, CredenciasLogin, and Claims structs to models package

--- api/main.go
+++ api/main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/GuilhermeFerza/CirculaSSA/controllers"
+	"github.com/GuilhermeFerza/CirculaSSA/models"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
@@ -16,37 +17,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type Vaga struct {
-	ID          int     `json:"id"`
-	Titulo      string  `json:"titulo" binding:"required,min=3,max=100"`
-	Descricao   string  `json:"descricao" binding:"required,min=10"`
-	Empresa     string  `json:"empresa" binding:"required"`
-	Tipo        string  `json:"tipo" binding:"required,oneof=CLT Estágio 'Jovem Aprendiz' Mutirão"`
-	Bairro      string  `json:"bairro" binding:"required"`
-	Latitude    float64 `json:"latitude" binding:"required"`
-	Longitude   float64 `json:"longitude" binding:"required"`
-	LinkContato string  `json:"link_contato" binding:"required,url"`
-}
-
-type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-	Name     string `json:"name" binding:"required"`
-	Type     string `json:"type" binding:"required"`
-}
-
-type CredenciasLogin struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
-
 var jwtKey = []byte("CirculaSSA821")
-
-type Claims struct {
-	Email string `json:"email"`
-	jwt.RegisteredClaims
-}
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -59,7 +30,7 @@ func AuthMiddleware() gin.HandlerFunc {
 		}
 
 		tokenStrings := strings.TrimPrefix(tokenHeader, "Bearer ")
-		claims := &Claims{}
+		claims := &models.Claims{}
 
 		token, err := jwt.ParseWithClaims(tokenStrings, claims, func(token *jwt.Token) (interface{}, error) {
 			return jwtKey, nil
